8ed02fde400aed411dabda43d9a9f82016326201
MostafaSaeed <MostafaSaeed@MOSTAFASAEED-PC>
2016-05-03 23:04:16 +0200

    added different schemes for different nationalities

---
+++ DesignPatternsCSharp.SimpleTaxCalculator/DifferentSchemesForNationalities/DifferentSchemesForNationalities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsCSharp.SimpleTaxCalculator
+{
+    public class DifferentSchemesForNationalities
+    {
+        public double calculate(double income,DateTime joiningDate,Nationality nationality)
+        {
+            double tax;
+            DateTime SixMonthsEarlier = DateTime.Now.AddMonths(-6);
+            if (joiningDate > SixMonthsEarlier)
+                return 0;
+            if(nationality == Nationality.US)
+            {
+                if (income < 2000)
+                    tax = income * 0.05;
+                else if (income < 5000)
+                    tax = income * 0.1;
+                else if (income < 10000)
+                    tax = income * 0.15;
+                else
+                    tax = income * 0.20;
+                
+                tax += income * 0.05;
+                return Math.Round(tax, 2);
+            }
+            else
+            {
+                if (income < 1500)
+                    tax = income * 0.05;
+                else if (income < 3000)
+                    tax = income * 0.1;
+                else if (income < 7000)
+                    tax = income * 0.15;
+                else
+                    tax = income * 0.20;
+                return Math.Round(tax, 2);
+            }
+        }
+    }
+}
---
+++ DesignPatternsCSharp.Tests/DifferentSchemesForNationalitiesTests.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatternsCSharp.SimpleTaxCalculator;
+namespace DesignPatternsCSharp.Tests
+{
+    [TestClass]
+    public class DifferentSchemesForNationalitiesTests
+    {
+        [TestMethod]
+        public void USCitizenWithIncomeLessThan2000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 1000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income,joiningDate,Nationality.US);
+            //tax scheme
+            double expectedTaxes = income * 0.05;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void USCitizenWithIncomeEquals2000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 2000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.1;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+
+        [TestMethod]
+        public void USCitizenWithIncomeBetween2000And5000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 3000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.1;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void USCitizenWithIncomeEquals5000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 5000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.15;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void USCitizenWithIncomeBetween5000And10000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 6000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.15;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void USCitizenWithIncomeEquals10000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 10000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.20;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void USCitizenWithIncomeMoreThan10000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 15000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.US);
+            double expectedTaxes = income * 0.20;
+            // federal taxes
+            expectedTaxes += income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeLessThan1500()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 1000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            //tax scheme
+            double expectedTaxes = income * 0.05;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeEquals1500()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 1500;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.1;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeBetween1500And3000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 2000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.1;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeEquals3000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 3000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.15;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeBetween3000And7000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 5000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.15;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeEquals7000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 7000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.20;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+        [TestMethod]
+        public void OtherCitizenWithIncomeMoreThan7000()
+        {
+            DifferentSchemesForNationalities calculator = new DifferentSchemesForNationalities();
+            double income = 7000;
+            DateTime joiningDate = DateTime.Now.AddMonths(-6);
+            double calculatedTaxes = calculator.calculate(income, joiningDate, Nationality.Canada);
+            double expectedTaxes = income * 0.20;
+            expectedTaxes = Math.Round(expectedTaxes, 2);
+            Assert.AreEqual(expectedTaxes, calculatedTaxes);
+        }
+
+    }
+}
